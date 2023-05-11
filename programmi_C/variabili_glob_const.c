996753077ecc832c1aebd5e3ad9b84be09821a88
Nicola Boscolo <nicolaboscolocegion@gmail.com>
2023-05-11 11:54:23 +0200

    variabili globali

---
+++ programmi_C/variabili_glob_const.c
@@ -0,0 +1,11 @@
+
+//questa e' una variabile globale
+// puo' essere vista da qualsisi parte del programma 
+int glob =10;
+
+int main()
+{
+    //questa e' una variabile locare si puo' utilizzare solo dentro alle {} in cui si trova
+    int locale =10;
+    return 0;
+}
